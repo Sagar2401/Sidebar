10b54ccc94a081772a69c0e6c37aec1a9ea136b3
Sagar2401 <prajaptisagar81410@gmail.com>
2023-12-01 13:02:47 +0530

    change auth

--- src/App.js
+++ src/App.js
@@ -1,50 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import {  Routes, Route,  } from "react-router-dom";
+import {  Routes, Route, useNavigate,  } from "react-router-dom";
 import Overview from "./pages/Overview";
 import { data } from "./components/SidebarData";
 import { Login } from "./pages/Login";
+import { getCookie } from "./Assets/coockie";
 
 function App() {
+const navigate = useNavigate()
+  const token = getCookie('authToken')
 
-  function organizeTabsIntoHierarchy(tabArray) {
-    const organizedTabs = {};
+useEffect(() => {
+ if (token){
+  navigate("/home")
+ }else(
+  navigate("/")
 
-    // Group tabs by parentId
-    tabArray.forEach((tab) => {
-      const parentId = tab.parentId;
-      if (!organizedTabs[parentId]) {
-        organizedTabs[parentId] = [];
-      }
-      organizedTabs[parentId].push(tab);
-    });
-
-    // Build hierarchy
-    function buildHierarchy(parentId) {
-      const children = organizedTabs[parentId];
-      if (children) {
-        return children.map((child) => {
-          const childWithChildren = {
-            ...child,
-            children: buildHierarchy(child.encryptedTabId),
-          };
-          return childWithChildren;
-        });
-      } else {
-        return [];
-      }
-    }
-
-    // Root level tabs (parentId = 0)
-    const rootTabs = organizedTabs["0"] || [];
-
-    // Build the hierarchy
-    const hierarchy = rootTabs.map((rootTab) => {
-      return { ...rootTab, children: buildHierarchy(rootTab.encryptedTabId) };
-    });
-
-    return hierarchy;
-  }
+ )
+}, [])
 
 
   return (
